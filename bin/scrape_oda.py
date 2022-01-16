d52b07291f55ca0420f3e24ba0ce02d61c1f3326
Joakim Hove <joakim.hove@gmail.com>
2022-01-16 11:21:40 +0100

    Add output format switch --fmt for json and txt

--- bin/scrape_oda.py
+++ bin/scrape_oda.py
@@ -1,4 +1,5 @@
 import sys
+import json
 from argparse import ArgumentParser
 
 from scraper import OdaScraper, Throttle, Fetcher
@@ -8,6 +9,7 @@ def run_main(argv):
     arg_parser = ArgumentParser()
     arg_parser.add_argument("--host", help="Hostname to start crawling", default="https://oda.com")
     arg_parser.add_argument("--max-bw", help="Maximum bandwidth byte/s - 0 : unlimited", default=0, type=int)
+    arg_parser.add_argument("--fmt", help="Output format txt|json", default="txt")
 
     args = arg_parser.parse_args(argv[1:])
 
@@ -17,8 +19,13 @@ def run_main(argv):
     os = OdaScraper(fetcher)
     pc = os.run()
 
-    with open("dump.txt", "w") as fh:
-        fh.write(pc.dump())
+    if args.fmt == "txt":
+        with open("dump.txt", "w") as fh:
+            fh.write(pc.dump())
+    elif args.fmt == "json":
+        with open("dump.json", "w") as fh:
+            fh.write(json.dumps(pc.data()))
+
 
 
 if __name__ == "__main__":
